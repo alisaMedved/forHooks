5cca53d124deb02f92a9311993430be683f193a0
Alice <adelaide3alice@gmail.com>
2021-06-24 18:49:24 +0500

    вспомнить + проверить хуки

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Test} from "./pages/test/Test4";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
